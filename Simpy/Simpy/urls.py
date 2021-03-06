154139483e10b95af95a5dedf1d9860e3488fe69
Roman <nahervihuher@gmail.com>
2021-03-06 21:02:35 +0300

    add regisret form

--- Simpy/Simpy/urls.py
+++ Simpy/Simpy/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as userViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('servicies.urls'))
+    path('', include('servicies.urls')),
+    path('register', userViews.register, name='register')
 ]
