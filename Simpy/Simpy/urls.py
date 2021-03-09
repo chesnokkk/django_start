0bf3d833a55aaaf9d8e4eab98c0ab4624dc38805
Chesnokkk <nahervihuher@gmail.com>
2021-03-09 15:05:03 +0300

    Make stability auth

--- Simpy/Simpy/urls.py
+++ Simpy/Simpy/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as userViews
+from django.contrib.auth import views as authViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('servicies.urls')),
-    path('register', userViews.register, name='register')
+    path('register', userViews.register, name='register'),
+    path('user', authViews.LoginView.as_view(template_name='users/user.html'), name='user'),
+    path('exit', authViews.LogoutView.as_view(template_name='users/exit.html'), name='exit'),
+    path('profile', userViews.profile, name='profile'),
 ]
